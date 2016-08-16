82f10eba5e7ac8d23dbfa7c2bb105927ebc18303
daniel <Daniel2_Huang@asus.com>
2016-08-16 12:08:22 +0800

    daniel test2

--- app/src/main/java/com/example/daniel2_huang/danieltest1/MainActivity.java
+++ app/src/main/java/com/example/daniel2_huang/danieltest1/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("daniel test2", "daniel test2");
         Log.d("daniel test3","daniel test3");
         Log.d("daniel test4","daniel test4");
+        Log.d("daniel test5","daniel test5");
     }
 
     @Override
